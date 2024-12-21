cfdf3487bc8acc9f405a0de9a96dc9eef1156816
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-12-21 15:30:50 +0500

    Added shadcn/ui & images and updated global.css & tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="max-w-[496px] mx-auto size-full flex flex-col py-10">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
