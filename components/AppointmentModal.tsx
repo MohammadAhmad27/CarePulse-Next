cfb6e7a7bd6a8818c34d5e7761a024258ac432b6
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-12-29 23:50:50 +0500

    Added <DataTable /> and <Appointment Modal />

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Appointment } from "@/types/appwrite.types";
+import AppointmentForm from "./forms/AppointmentForm";
+
+export const AppointmentModal = ({
+  patientId,
+  userId,
+  appointment,
+  type,
+}: {
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+  type: "schedule" | "cancel";
+}) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+        >
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-dark-400 border-dark-500 sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
+          <DialogDescription>
+            Please fill in the following details to {type} appointment
+          </DialogDescription>
+        </DialogHeader>
+
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          type={type}
+          appointment={appointment}
+          setOpen={setOpen}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
